5881c2202504167a960d3ebe3f863e3eb40d6c0d
hachchi <161139489+hachchi@users.noreply.github.com>
2024-03-07 13:19:52 +1100

    docs: update RelatedWords.js

--- com-dict-client/src/components/AddWord/RelatedWords.js
+++ com-dict-client/src/components/AddWord/RelatedWords.js
@@ -1,41 +1,64 @@
 import React, { useState } from "react";
 import { Tag, Input, Tooltip, Button } from "antd";
 
+/**
+ * EditableTagGroup component allows users to manage tags dynamically.
+ * @param {function} onChange - Callback function triggered when tags are changed.
+ */
 function EditableTagGroup({ onChange }) {
+  // State variables to manage tags and input visibility
   const [tags, setTags] = useState([]);
   const [inputVisible, setInputVisible] = useState(false);
   const [inputValue, setInputValue] = useState("");
   const [input, setInput] = useState("");
-  console.log(input);
+
+  /**
+   * Handle tag removal.
+   * @param {string} removedTag - The tag to be removed.
+   */
   const handleClose = (removedTag) => {
     const newTags = tags.filter((tag) => tag !== removedTag);
     setTags(newTags);
   };
 
+  /**
+   * Show the input field for adding a new tag.
+   */
   const showInput = () => {
     setInputVisible(true);
   };
 
+  /**
+   * Handle input change event.
+   * @param {object} e - Event object containing the input value.
+   */
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
   };
 
+  /**
+   * Handle input confirmation for adding a new tag.
+   */
   const handleInputConfirm = () => {
     let allTags = [];
     if (inputValue && tags.indexOf(inputValue) === -1) {
       allTags = [...tags, inputValue];
     }
-    console.log(allTags);
-    onChange(tags);
+    onChange(allTags);
     setTags([]);
     setInputValue("");
     setInputVisible(false);
   };
 
+  /**
+   * Save the input reference.
+   * @param {object} val - Input element reference.
+   */
   const saveInputRef = (val) => setInput(val);
 
   return (
     <div>
+      {/* Display existing tags */}
       {tags.map((tag, index) => {
         const isLongTag = tag.length > 20;
         const tagElem = (
@@ -51,11 +74,12 @@ function EditableTagGroup({ onChange }) {
           tagElem
         );
       })}
+      {/* Display input field for adding new tags */}
       {inputVisible && (
         <Input
           ref={saveInputRef}
           type="text"
-          size="larger"
+          size="large"
           style={{ width: 90 }}
           value={inputValue}
           onChange={handleInputChange}
@@ -63,6 +87,7 @@ function EditableTagGroup({ onChange }) {
           onPressEnter={handleInputConfirm}
         />
       )}
+      {/* Display button to show the input field */}
       {!inputVisible && (
         <Button size="medium" type="dashed" onClick={showInput}>
           + New Word
