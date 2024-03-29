74b20d299d7760cc711a97f57843064a10727bf5
hachchi <161139489+hachchi@users.noreply.github.com>
2024-03-30 00:56:57 +1100

    docs: update catHead.js

--- com-dict-client/src/components/Categories/catHead.js
+++ com-dict-client/src/components/Categories/catHead.js
@@ -1,18 +1,23 @@
 import React from "react";
-import { Row, Col} from 'antd';
+import { Row, Col } from "antd";
 import { Typography } from "antd";
-
 
 const { Title } = Typography;
 
 function CatHead() {
   return (
+    // Row to contain the title with a single column layout
     <Row>
-    <Col span={24}>
-    <div className="cat_title">
-      <Title level={1} style={{ color: "white" }}>Sports</Title>
-    </div>
-    </Col>
+      {/* Column spanning the entire width */}
+      <Col span={24}>
+        {/* Container div with class 'cat_title' */}
+        <div className="cat_title">
+          {/* Title component with level 1 and white color */}
+          <Title level={1} style={{ color: "white" }}>
+            Sports
+          </Title>
+        </div>
+      </Col>
     </Row>
   );
 }
